17f423565350713e488e880d73ef0470a68c68b7
sanim-ratul <2020294@iub.edu.bd>
2023-12-13 12:09:50 +0600

    save8

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const apiKey = "YOUR_API_KEY_HERE"; // Replace with your actual API key
-
-function getWeather() {
-  const cityName = document.getElementById("city-search").value.trim();
-  if (!cityName) {
-    alert("Please enter a city name!");
-    return;
-  }
-
-  const url = `https://api.openweathermap.org/data/3.0/onecall?q=${cityName}&appid=${apiKey}`;
-
-  const weatherDescElement = weatherDataContainer.querySelector("#weather-desc");
+const searchButton = document.getElementById("search-button");
+const searchCountry = document.getElementById("search-country");
+const weatherDataContainer = document.getElementById("weather-data");
+const cityNameElement = weatherDataContainer.querySelector("#city-name");
+const currentTempElement = weatherDataContainer.querySelector("#current-temp");
+const weatherDescElement = weatherDataContainer.querySelector("#weather-desc");
 const weatherIconElement = weatherDataContainer.querySelector("#weather-icon");
 const forecastListElement = weatherDataContainer.querySelector("#forecast-list");
 const feelsLikeElement = weatherDataContainer.querySelector("#feels-like");
@@ -17,22 +11,54 @@ const humidityElement = weatherDataContainer.querySelector("#humidity");
 const windSpeedElement = weatherDataContainer.querySelector("#wind-speed");
 
 function getWeatherData(city) {
-  const url = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&appid=YOUR_API_KEY&units=metric`;
+  const url = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&appid=3d8d86731b8c3cac8b066ac1087a0bd4&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       // Update city name
       cityNameElement.innerText = data.city.name;
 
-      sync function searchWeather() {
-        const apiKey = 'Javascript';
-        const cityInput = document.getElementById('search-input').value;
+      // Update current temperature and description
+      currentTempElement.innerText = `${Math.round(data.current.temp)}°C`;
+      weatherDescElement.innerText = data.current.weather[0].main;
 
-        try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${5a4a244b150449ea4c8ac5fe159deb2a}&units=metric`);
-            const data = await response.json();
+      // Update weather icon based on current condition
+      weatherIconElement.classList.remove("sunny", "cloudy", "rainy", "stormy");
+      if (data.current.weather[0].main.includes("Sun")) {
+        weatherIconElement.classList.add("sunny");
+      } else if (data.current.weather[0].main.includes("Cloud")) {
+        weatherIconElement.classList.add("cloudy");
+      } else if (data.current.weather[0].main.includes("Rain")) {
+        weatherIconElement.classList.add("rainy");
+      } else if (data.current.weather[0].main.includes("Storm")) {
+        weatherIconElement.classList.add("stormy");
+      }
 
-            document.getElementById('city').innerText = `City: ${data.name}`;
-            document.getElementById('temperature').innerText = `Temperature: ${data.main.temp} °C`;
-            document.getElementById('description').innerText = `Description: ${data.weather[0].description}`;
-            document.getElementById('weather-icon').src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      // Clear previous forecast data
+      forecastListElement.innerHTML = "";
+
+      // Update forecast for next 3 days
+      for (let i = 1; i <= 3; i++) {
+        const day = data.daily[i];
+        const dayElement = document.createElement("li");
+        dayElement.innerText = `${day.dt_txt.slice(0, 10)}: ${Math.round(day.temp.day)}°C, ${day.weather[0].main}`;
+        forecastListElement.appendChild(dayElement);
+      }
+
+      // Update additional information
+      feelsLikeElement.innerText = `${Math.round(data.current.feels_like)}°C`;
+      humidityElement.innerText = `${data.current.humidity}%`;
+      windSpeedElement.innerText = `${data.current.wind_speed} m/s`;
+    })
+    .catch((error) => {
+      console.error(error);
+      weatherDataContainer.innerHTML = "<p>27c</p>";
+	  weatherDataContainer.innerHTML = "<p>28c</p>";
+    });
+}
+
+searchButton.addEventListener("click", () => {
+  const city = searchCountry.value.trim();
+  if (!city) return;
+  getWeatherData(city);
+});
