2d79e2368e63d332bd4bab189cf7729a035cf392
sanim-ratul <2020294@iub.edu.bd>
2023-12-13 11:30:24 +0600

    save5

--- index.js
+++ index.js
@@ -23,3 +23,16 @@ function getWeatherData(city) {
     .then((data) => {
       // Update city name
       cityNameElement.innerText = data.city.name;
+
+      sync function searchWeather() {
+        const apiKey = 'Javascript';
+        const cityInput = document.getElementById('search-input').value;
+
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${5a4a244b150449ea4c8ac5fe159deb2a}&units=metric`);
+            const data = await response.json();
+
+            document.getElementById('city').innerText = `City: ${data.name}`;
+            document.getElementById('temperature').innerText = `Temperature: ${data.main.temp} °C`;
+            document.getElementById('description').innerText = `Description: ${data.weather[0].description}`;
+            document.getElementById('weather-icon').src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
